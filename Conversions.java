9d8fcb65b913c952b9481488837fc744cfa5aad7
C E <ceisner01@bvsd.org>
2025-08-20 18:57:07 +0000

    V1.0 - Added menu options and main scaffold

--- Conversions.java
+++ Conversions.java
@@ -1,1 +1,19 @@
+// Author: Colin
+// 08/20/2025
 
+// Conversions class
+public class Conversions{
+    public static void main(String[] args){
+        // Menu options for the conversions
+        System.out.println("1. Fahrenheit to Celsius");
+        System.out.println("2. Celsius to Fahrenheit");
+        System.out.println("3. Feet to Meters");
+        System.out.println("4. Meters to Feet");
+        System.out.println("5. Ounces to Milliliters");
+        System.out.println("6. Milliliters to Ounces");
+        System.out.println("7. ");
+        System.out.println("8. ");
+        System.out.println("9. ");
+        System.out.println("10. ");
+    }
+}
